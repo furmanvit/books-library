1b231bb8ac7cc752fdf3bf4ef2ea374fe46e53fb
furmanvit <furmanvit@gmail.com>
2017-12-13 00:01:32 +0200

    delete route.config, page-not-found.component, clear app.module.ts

--- library-web/src/app/app.module.ts
+++ library-web/src/app/app.module.ts
@@ -10,8 +10,6 @@ import { SiteHeaderComponent } from './components/site-header/site-header.compon
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookComponent } from './components/book/book.component';
 import { BookPopupComponent } from './components/book-popup/book-popup.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { routing, appRoutingProviders } from './config/routes.config';
 import { BookService } from './services/book/book.service';
 import { NetService } from './services/net/net.service';
 import { CapitalizePipe } from './components/pipes/capitalize.pipe';
@@ -25,19 +23,17 @@ import { CapitalizePipe } from './components/pipes/capitalize.pipe';
     BookListComponent,
     BookComponent,
     BookPopupComponent,
-    PageNotFoundComponent,
     CapitalizePipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    routing,
     HttpClientModule,
     AlertModule.forRoot(),
     ModalModule.forRoot(),
     
   ],
-  providers: [appRoutingProviders,
+  providers: [
     /* Services */
     BookService,
     NetService
